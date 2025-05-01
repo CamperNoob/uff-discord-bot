397b90d820dcb2a1d2687f4b1fc866cf23cd919d
Misha Yudin <misha.yudin19@gmail.com>
2025-05-01 16:11:41 +0300

    Fixed tooltip for it to fit

--- translations/en.py
+++ translations/en.py
@@ -71,8 +71,9 @@ GRAFANA_INVITE_SUCCESS = 'Invite link created successfully'
 GRAFANA_INVITE_USER_ALREADY_EXISTS = f"{ERROR_GENERIC}: User already exists in Grafana with name"
 
 MATCH_HISTORY_ADD_DESCRIPTION = 'Adds match record into match history table of Grafana'
-MATCH_HISTORY_ADD_PARAMETER_DESCRIPTION = 'Format: date day.month.year;layer;opponent;mercs;our tickers/opp R1;our tickets/opp R2;link to event channel;vods;tactics'
+MATCH_HISTORY_ADD_PARAMETER_DESCRIPTION = 'day.month.year;layer;opponent;mercs;tickers/opp R1;tickets/opp R2;link to event;vods;tactics'
 MATCH_HISTORY_ADD_DATA_PARSE_ERROR = f"{ERROR_GENERIC}: Error parsing the data. Details"
+MATCH_HISTORY_ADD_DUPLICATE_RECORD_ERROR = f'{ERROR_GENERIC}: Error adding record to database. Record already exists for event'
 MATCH_HISTORY_ADD_SUCCESS_TEXT = "Successfully added match record to database"
 MATCH_HISTORY_ADD_SUCCESS_EVENT_NAME = "Match name"
 MATCH_HISTORY_ADD_SUCCESS_DATE = "Match date"
